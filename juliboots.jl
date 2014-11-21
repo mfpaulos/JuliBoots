9b05a2501edebdba7765e91e7f375ae08715e345
Miguel F. Paulos <miguel.paulos@gmail.com>
2014-11-21 23:35:21 +0100

    Added juliboots.jl, shifted common into main

---
+++ juliboots.jl
@@ -0,0 +1,12 @@
+module juliboots
+println("JuliBootS: a package for numerical bootstrap computations.")
+println("Version: 1.0")
+
+using various
+using main
+
+export chooseTable, setupLP, bissect, value, dropOdd!, changeTarget!,dropEven!,opemax
+export filter,iterate!,status,cost,solution,makeVector # LP routines, this makes them accessible when 'using main'
+
+end
+
